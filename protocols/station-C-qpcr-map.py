a920a2b629e63675d90f3ae151efbd1aeacf727e
Anton Jackson-Smith <acjs@stanford.edu>
2020-03-18 12:21:43 -0700

    New protocols

---
+++ protocols/station-C-qpcr-map.py
@@ -0,0 +1,136 @@
+metadata = {
+    'protocolName': 'BP Genomics Station C',
+    'author': 'Chaz <chaz@opentrons.com; Anton <acjs@stanford.edu>',
+    'source': 'COVID-19 Project',
+    'apiLevel': '2.2'
+}
+
+# Protocol constants
+
+# Master mix locations on the eppendorf tube holder
+REAGENT_LOCATIONS = {
+    'A1': 'Endogenous',
+    'A2': 'Reaction',
+    'A3': 'Standard',
+    'A4': 'Water',
+    'C1': 'Control RNA',
+    'C2': 'Control RNA 2',
+    'C5': 'PCD 8',
+    'C6': 'PCD 7',
+    'D1': 'PCD 6',
+    'D2': 'PCD 5',
+    'D3': 'PCD 4',
+    'D4': 'PCD 3',
+    'D5': 'PCD 2',
+    'D6': 'PCD 1'
+}
+
+# Number of samples
+# We expect that inbound samples are ordered numerically in well order
+# Sample 1 in well 1, sample 2 in well 2, etc.
+NUM_SAMPLES = 8
+
+# Tip locations
+SAMPLE_TIP_LOCATIONS = ['2', '3']
+
+MASTER_MIX_MAP = '''
+Endogenous	Reaction	Reaction	Reaction	Endogenous	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Endogenous	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Reaction	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Reaction	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Standard	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Standard	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Reaction	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+Endogenous	Reaction	Reaction	Reaction	Reaction	Reaction	Reaction	Standard	Standard	Standard	Standard	Standard
+'''
+
+SAMPLE_MAP = '''
+Sample 1	Sample 1	Sample 1	Sample 1	Control RNA	Water	PCD 1	Water	PCD 1	Water	PCD 1	Water
+Sample 2	Sample 2	Sample 2	Sample 2	Control RNA 2	Water	PCD 2	Water	PCD 2	Water	PCD 2	Water
+Sample 3	Sample 3	Sample 3	Sample 3	Control RNA	Water	PCD 3	Water	PCD 3	Water	PCD 3	Water
+Sample 4	Sample 4	Sample 4	Sample 4	Control RNA 2	Water	PCD 4	Water	PCD 4	Water	PCD 4	Water
+Sample 5	Sample 5	Sample 5	Sample 5	Control RNA	Water	PCD 5	Water	PCD 5	Water	PCD 5	Water
+Sample 6	Sample 6	Sample 6	Sample 6	Control RNA 2	Water	PCD 6	Water	PCD 6	Water	PCD 6	Water
+Sample 7	Sample 7	Sample 7	Sample 7	Control RNA	Water	PCD 7	Water	PCD 7	Water	PCD 7	Water
+Sample 8	Sample 8	Sample 8	Sample 8	Control RNA 2	Water	PCD 8	Water	PCD 8	Water	PCD 8	Water
+'''
+
+import re
+
+def transfer_with_primitives(p, source, dest, volume=5, mix=19):
+    p.pick_up_tip()
+    p.aspirate(volume, source)
+    p.dispense(volume - 1, dest)
+    for _ in range(4):
+        p.aspirate(mix, dest)
+        p.dispense(mix, dest)
+    p.dispense(1, dest)
+    p.blow_out(dest.top())
+    p.drop_tip()
+
+def run(protocol):
+    sample_tip_racks = [
+        protocol.load_labware(
+            'opentrons_96_filtertiprack_20ul', s) for s in SAMPLE_TIP_LOCATIONS
+            ]
+    p20 = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=sample_tip_racks)
+
+    tempdeck = protocol.load_module('tempdeck', '4')
+    tempdeck.set_temperature(4)
+
+    tempplate = tempdeck.load_labware(
+        'opentrons_96_aluminumblock_nest_wellplate_100ul')
+
+    tempplate_wells_by_row = [*x for x in tempplate.rows()]
+
+    reagent_rack = protocol.load_labware(
+        'opentrons_24_tuberack_nest_1.5ml_snapcap', 5)
+
+    elution_plate = protocol.load_labware(
+        'nest_96_wellplate_100ul_pcr_full_skirt', '1', 'Elution Plate')
+
+    p20.flow_rate.aspirate = 10
+    p20.flow_rate.dispense = 15
+    p20.flow_rate.blow_out = 50
+
+    # Distribute Master Mixes
+    # Split up the master mix map into a list
+    master_mix_labels = re.split(r'[\n\t]+', MASTER_MIX_MAP.strip())
+    master_mix_wells = dict()
+
+    # Figure out unique master mixes on the map, so we can use a single
+    # tip for each
+    for mix in set(master_mix_labels):
+        master_mix_wells[mix] = list()
+    for mix, well in zip(master_mix_labels, tempplate_wells_by_row):
+        master_mix_wells[mix].append(well)
+
+    # Do the master mix transfer
+    for mix, wells in master_mix_wells.items():
+        p20.pick_up_tip()
+        for well in wells:
+            p20.transfer(15, reagent_rack[REAGENT_LOCATIONS[mix]], well, new_tip='never')
+            p20.blow_out(well.top())
+        p20.drop_tip()
+
+    # Transfer the samples and controls
+    # Same deal as above, except every transfer gets its own tip,
+    # which makes it easier because we don't bother optimizing for tip use.
+    # Transfers are made row by row, left to right.
+
+    sample_labels =  re.split(r'[\n\t]+', SAMPLE_MAP.strip())
+    sample_wells = zip(sample_labels, tempplate_wells_by_row)
+    for sample, dest_well in sample_wells:
+        # Determine whether we are dealing with an actual sample, which we
+        # will take from the input sample plate; or a control, which we will
+        # take from a location on the reagent rack. We expect samples to be
+        # numbered, and will take the sample from the well matching the sample
+        # number (eg Sample 1 = well 1 = plate.wells()[0])
+
+        sample_match = re.match(r'Sample ([0-9]+)', sample)
+        if sample_match:
+            source_well = elution_plate.wells()[int(sample_match.groups()[0]) - 1]
+        else:
+            source_well = reagent_rack[REAGENT_LOCATIONS[sample]]
+
+        transfer_with_primitives(p20, source_well, dest_well)
--- protocols/station-C-qpcr-validation.py
+++ protocols/station-C-qpcr-validation.py
@@ -14,8 +14,13 @@ STANDARD_CURVE_MIX_LOCATION = 'A3'
 WATER_LOCATION = 'A4'
 
 # Standard locations
-INTERNAL_CONTROL_RNA_TUBES
-STANDARD_CURVE_TUBES = []
+INTERNAL_CONTROL_RNA_TUBES = [
+    'C1', 'C2'
+]
+
+STANDARD_CURVE_TUBES = [
+    'D6', 'D5', 'D4', 'D3', 'D2', 'D1', 'C6', 'C5'
+]
 
 # Number of samples
 # We expect that inbound samples are ordered numerically in well order
@@ -23,25 +28,20 @@ STANDARD_CURVE_TUBES = []
 NUM_SAMPLES = 8
 
 # Tip locations
-MASTERMIX_TIP_LOCATIONS = ['3']
-SAMPLE_TIP_LOCATIONS = ['2']
+SAMPLE_TIP_LOCATIONS = ['2', '3']
 
-def transfer_with_primitives(source, dest, volume=5, mix=19):
-    p20.pick_up_tip()
-    p20.aspirate(volume, source)
-    p20.dispense(volume-1, dest)
+def transfer_with_primitives(p, source, dest, volume=5, mix=19):
+    p.pick_up_tip()
+    p.aspirate(volume, source)
+    p.dispense(volume - 1, dest)
     for _ in range(4):
-        p20.aspirate(mix, dest)
-        p20.dispense(mix, dest)
-    p20.dispense(1, dest)
-    p20.blow_out(dest.top())
-    p20.drop_tip()
+        p.aspirate(mix, dest)
+        p.dispense(mix, dest)
+    p.dispense(1, dest)
+    p.blow_out(dest.top())
+    p.drop_tip()
 
 def run(protocol):
-    mastermix_tip_rack = protocol.load_labware(
-            'opentrons_96_filtertiprack_20ul', MASTERMIX_TIP_LOCATIONS[0]) #for s in MASTERMIX_TIP_LOCATIONS
-    mastermix_tip = mastermix_tip_rack.wells()[0]
-
     sample_tip_racks = [
         protocol.load_labware(
             'opentrons_96_filtertiprack_20ul', s) for s in SAMPLE_TIP_LOCATIONS
@@ -49,7 +49,7 @@ def run(protocol):
     p20 = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=sample_tip_racks)
 
     tempdeck = protocol.load_module('tempdeck', '4')
-    tempdeck.set_temperature(4)
+    # tempdeck.set_temperature(4)
 
     tempplate = tempdeck.load_labware(
         'opentrons_96_aluminumblock_nest_wellplate_100ul')
@@ -92,7 +92,11 @@ def run(protocol):
     p20.pick_up_tip()
     standard_curve_wells = [
         tempplate['E5'], tempplate['F5'],
-        *tempplate.columns()[7:11]
+        *tempplate.columns()[7],
+        *tempplate.columns()[8],
+        *tempplate.columns()[9],
+        *tempplate.columns()[10],
+        *tempplate.columns()[11]
     ]
 
     for well in standard_curve_wells:
@@ -109,26 +113,28 @@ def run(protocol):
     sample_rna = elution_plate.wells()[:NUM_SAMPLES]
     for sample_number, sample_well in enumerate(sample_rna):
         for i in range(4):
-            sample_destination = tempplate.rows()[0][i]
-            transfer_with_primitives(sample_well, sample_destination)
+            sample_destination = tempplate.rows()[sample_number][i]
+            transfer_with_primitives(p20, sample_well, sample_destination)
 
     # Transfer control RNA into the control RNA wells
-    for i in range(6):
-        control_rna = INTERNAL_CONTROL_RNA[i // 2]
-        transfer_with_primitives(control_rna_well, tempplate.columns(4)[i])
+    for i in range(8):
+        control_rna_well = reagent_rack[INTERNAL_CONTROL_RNA_TUBES[i % 2]]
+        transfer_with_primitives(p20, control_rna_well, tempplate.columns()[4][i])
 
     # Transfer water into the negative control wells
     negative_control_wells = [
-        tempplate['G5'], tempplate['H5'],
-        *tempplate.columns(5),
-        *tempplate.columns(7),
-        *tempplate.columns(9),
-        *tempplate.columns(11)
+        *tempplate.columns()[5],
+        *tempplate.columns()[7],
+        *tempplate.columns()[9],
+        *tempplate.columns()[11]
     ]
     for well in negative_control_wells:
-        transfer_with_primitives(WATER_LOCATION, well)
+        transfer_with_primitives(p20, reagent_rack[WATER_LOCATION], well)
 
     # Transfer standard curve into standard curve wells
     for curve_column in [6, 8, 10]:
         for dilution in range(8):
-            transfer_with_primitives(STANDARD_CURVE_TUBES[dilution], tempplate.columns()[curve_column][dilution])
+            transfer_with_primitives(
+                p20,
+                reagent_rack[STANDARD_CURVE_TUBES[dilution]],
+                tempplate.columns()[curve_column][dilution])
