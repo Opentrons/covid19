7e0e1f54d3747c681552cdd1262f6300ab22af49
Nick Diehl <ndiehl@opentrons.com>
2020-04-08 13:39:08 -0400

    changes number of mastermixes in S2 station C to n=1, removes +/- controls

--- protocols/S2/v1_station_c_S2.ot2.apiv2.py
+++ protocols/S2/v1_station_c_S2.ot2.apiv2.py
@@ -12,7 +12,7 @@ metadata = {
 REAGENT SETUP:
 
 - slot 5 2ml tuberack:
-    - mastermixes: tubes A1-A3
+    - mastermixes: tube A1
     - positive control: tube B1
     - negative control: tube B2
 """
@@ -41,50 +41,20 @@ def run(ctx: protocol_api.ProtocolContext):
 
     # setup up sample sources and destinations
     samples = source_plate.wells()[:NUM_SAMPLES]
-    sample_dest_sets = [
-        row[i*3:(i+1)*3]
-        for i in range(12//3)
-        for row in pcr_plate.rows()
-    ][:NUM_SAMPLES]
-    mm = tuberack.rows()[0][:3]
-    mm_dests = [
-        [well for col in pcr_plate.columns()[j::3]
-            for well in col][:NUM_SAMPLES] + pcr_plate.columns()[9+j][-2:]
-        for j in range(3)
-    ]
-    pos_control = tuberack.rows()[1][0]
-    pos_control_dests = pcr_plate.rows()[-2][-1*3:]
-    neg_control = tuberack.rows()[1][1]
-    neg_control_dests = tuberack.rows()[-1][-1*3:]
+    dests = pcr_plate.wells()[:NUM_SAMPLES]
+    mm = tuberack.rows()[0]
 
-    # transfer mastermixes
-    for s, d_set in zip(mm, mm_dests):
-        p20.pick_up_tip()
-        for d in d_set:
-            p20.transfer(20, s, d, new_tip='never')
-            p20.blow_out(d.bottom(5))
-        p20.drop_tip()
+    # transfer mastermix
+    p20.pick_up_tip()
+    for d in dests:
+        p20.transfer(20, mm, d, new_tip='never')
+        p20.blow_out(d.bottom(5))
+    p20.drop_tip()
 
     # transfer samples to corresponding locations
-    for s, d_set in zip(samples, sample_dest_sets):
-        for d in d_set:
-            p20.pick_up_tip()
-            p20.transfer(5, s, d, new_tip='never')
-            p20.mix(1, 10, d)
-            p20.aspirate(5, d.top(2))
-            p20.drop_tip()
-
-    # transfer controls
-    for d in pos_control_dests:
+    for s, d in zip(samples, dests):
         p20.pick_up_tip()
-        p20.transfer(5, pos_control, d, new_tip='never')
-        p20.mix(1, 10, d)
-        p20.aspirate(5, d.top(2))
-        p20.drop_tip()
-
-    for d in neg_control_dests:
-        p20.pick_up_tip()
-        p20.transfer(5, neg_control, d, new_tip='never')
+        p20.transfer(5, s, d, new_tip='never')
         p20.mix(1, 10, d)
         p20.aspirate(5, d.top(2))
         p20.drop_tip()
