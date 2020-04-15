4267bd7647690439cf5f3c4203d7e250193dfc91
Nick Diehl <ndiehl@opentrons.com>
2020-04-14 22:06:18 -0400

    adds tip tracking to S2 station C

--- protocols/S2/v2_station_c_S2.ot2.apiv2.py
+++ protocols/S2/v2_station_c_S2.ot2.apiv2.py
@@ -1,4 +1,6 @@
 from opentrons import protocol_api
+import os
+import json
 
 # metadata
 metadata = {
@@ -17,8 +19,9 @@ REAGENT SETUP:
     - negative control: tube B2
 """
 
-NUM_SAMPLES = 8
+NUM_SAMPLES = 30
 VOLUME_MMIX = 20
+TIP_TRACK = False
 
 
 def run(ctx: protocol_api.ProtocolContext):
@@ -34,8 +37,7 @@ def run(ctx: protocol_api.ProtocolContext):
         'chilled 2ml screw tube block for mastermix')
     tips20 = [
         ctx.load_labware('opentrons_96_filtertiprack_20ul', slot)
-        for slot in ['2', '5', '8']
-    ]
+        for slot in ['2', '3', '5', '6', '8', '9', '10', '11']]
 
     # pipette
     p20 = ctx.load_instrument('p20_single_gen2', 'right', tip_racks=tips20)
@@ -45,16 +47,46 @@ def run(ctx: protocol_api.ProtocolContext):
     dests = pcr_plate.wells()[:NUM_SAMPLES]
     mm = tuberack.rows()[0]
 
+    tip_log = {'count': {}}
+    folder_path = 'C'
+    file_path = folder_path + '/tip_log.json'
+    # if TIP_TRACK and not ctx.is_simulating():
+    if os.path.isfile(file_path):
+        with open(file_path) as json_file:
+            data = json.load(json_file)
+            if 'tips20' in data:
+                tip_log['count'][p20] = data['tips20']
+            else:
+                tip_log['count'][p20] = 0
+    else:
+        tip_log['count'] = {p20: 0}
+    # else:
+    #     tip_log['count'] = {p20: 0}
+
+    tip_log['tips'] = {p20: [tip for rack in tips20 for tip in rack.wells()]}
+    tip_log['max'] = {p20: len(tip_log['tips'][p20])}
+
+    def pick_up(pip):
+        nonlocal tip_log
+        if tip_log['count'][pip] == tip_log['max'][pip]:
+            ctx.pause('Replace ' + str(pip.max_volume) + 'µl tipracks before \
+resuming.')
+            pip.reset_tipracks()
+            tip_log['count'][pip] = 0
+        pip.pick_up_tip(tip_log['tips'][pip][tip_log['count'][pip]])
+        tip_log['count'][pip] += 1
+
     # ensure volume does not reach the filter of the tip
     max_trans_per_asp = 230//(VOLUME_MMIX+5)  # max tip volume of 230µl
 
-    # split destination wells into sets that can be distributed to with 1 aspiration
+    # split destination wells into sets that can be distributed to with 1
+    # aspiration
     split_ind = [ind for ind in range(0, NUM_SAMPLES, max_trans_per_asp)]
     dest_sets = [dests[split_ind[i]:split_ind[i+1]]
                  for i in range(len(split_ind)-1)] + [dests[split_ind[-1]:]]
 
     # distribute to each set of wells using the same tip for each
-    p20.pick_up_tip()
+    pick_up(p20)
     for set in dest_sets:
         p20.distribute(VOLUME_MMIX, mm, [d.bottom(2) for d in set],
                        air_gap=5, disposal_volume=0, new_tip='never')
@@ -62,8 +94,17 @@ def run(ctx: protocol_api.ProtocolContext):
 
     # transfer samples to corresponding locations
     for s, d in zip(samples, dests):
-        p20.pick_up_tip()
+        pick_up(p20)
         p20.transfer(5, s, d, new_tip='never')
         p20.mix(1, 10, d)
         p20.aspirate(5, d.top(2))
         p20.drop_tip()
+
+    # track final used tip
+    # if TIP_TRACK and not ctx.is_simulating():
+    if not os.path.isdir(folder_path):
+        os.mkdir(folder_path)
+    data = {
+        'tips20': tip_log['count'][p20]}
+    with open(file_path, 'w') as outfile:
+        json.dump(data, outfile)
