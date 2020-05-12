78adf20e39fe7f72d61ec153028f79807efebd34
Nick Diehl <ndiehl@opentrons.com>
2020-05-12 13:47:39 -0400

    catches error in S7 station C BP MM creation volume

--- protocols/S7/v1_station_c_S7_bp_P20MULTI.ot2.apiv2.py
+++ protocols/S7/v1_station_c_S7_bp_P20MULTI.ot2.apiv2.py
@@ -91,7 +91,7 @@ resuming.')
     """ mastermix component maps """
     mm_tube = tube_block.wells()[0]
     mm_dict = {
-        'volume': 15,
+        'volume': 12,
         'components': {
             tube: vol
             for tube, vol in zip(tube_block.columns()[1][:2], [10, 2])
